88cb27185e0b238880f7c9d63d59c88ccebaf04b
Abiye12 <a.opusunju2269@student.leedsbeckett.ac.uk>
2026-03-13 18:48:16 +0000

    Add the MainPage layout structure (just the Column and Row skeleton

--- Quadrants/app/src/main/java/com/example/quadrants/MainActivity.kt
+++ Quadrants/app/src/main/java/com/example/quadrants/MainActivity.kt
@@ -1,28 +1,13 @@
 package com.example.quadrants
 
-import androidx.compose.ui.graphics.Color
 import android.os.Bundle
-import androidx.compose.foundation.Image
-import androidx.compose.ui.res.painterResource
-import androidx.compose.foundation.layout.size
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.Alignment
-import androidx.compose.material3.Text
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.quadrants.ui.theme.QuadrantsTheme
 import androidx.compose.foundation.background
@@ -54,76 +39,19 @@ class MainActivity : ComponentActivity() {
         }
     }
 }
-@Composable
-fun QuadrantCard(
-    title: String,
-    description: String,
-    backgroundColor: Color,
-    imageRes: Int,
-    modifier: Modifier = Modifier
-) {
-    Column(
-        modifier = modifier
-            .fillMaxSize()
-            .background(backgroundColor)
-            .padding(16.dp),
-        verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally
-    ) {
-        Image(
-            painter = painterResource( imageRes),
-            contentDescription = title,
-            modifier = Modifier.size(100.dp)
-        )
 
-        Text(
-            text = title,
-            fontWeight = FontWeight.Bold,
-            modifier = Modifier.padding(top = 16.dp, bottom = 16.dp)
-        )
-        Text(
-            text = description,
-            textAlign = TextAlign.Center
-        )
-    }
-}
 @Composable
 fun MainPage(modifier: Modifier = Modifier) {
     Column(modifier = modifier.fillMaxSize()) {
         Row(modifier = Modifier.weight(1f)) {
-            QuadrantCard(
-                title = stringResource(R.string.first_title),
-                description = stringResource(R.string.first_description),
-                backgroundColor = Color(0xFFEADDFF),
-                imageRes = R.drawable.nigerian_flag,
-                modifier = Modifier.weight(1f)
-            )
-            QuadrantCard(
-                title = stringResource(R.string.second_title),
-                description = stringResource(R.string.second_description),
-                backgroundColor = Color(0xFFD0BCFF),
-                imageRes = R.drawable.united_state_flag,
-                modifier = Modifier.weight(1f)
-            )
         }
         Row(modifier = Modifier.weight(1f)) {
-            QuadrantCard(
-                title = stringResource(R.string.third_title),
-                description = stringResource(R.string.third_description),
-                backgroundColor = Color(0xFFB69DF8),
-                imageRes = R.drawable.united_kingdom_flag,
-                modifier = Modifier.weight(1f)
-            )
-            QuadrantCard(
-                title = stringResource(R.string.fourth_title),
-                description = stringResource(R.string.fourth_description),
-                backgroundColor = Color(0xFFF6EDFF),
-                imageRes = R.drawable.russia_flag,
-                modifier = Modifier.weight(1f)
-            )
         }
     }
+
 }
+
+
 
 @Preview(showBackground = true)
 @Composable
