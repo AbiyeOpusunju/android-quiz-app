d593e39beae6f3fb7878024549ae5f66d7643901
Abiye12 <a.opusunju2269@student.leedsbeckett.ac.uk>
2026-03-16 13:49:34 +0000

    Add topping options to DataSource

--- Cupcake/app/src/main/java/com/example/cupcake/data/DataSource.kt
+++ Cupcake/app/src/main/java/com/example/cupcake/data/DataSource.kt
@@ -29,6 +29,16 @@ object DataSource {
     val quantityOptions = listOf(
         Pair(R.string.one_cupcake, 1),
         Pair(R.string.six_cupcakes, 6),
-        Pair(R.string.twelve_cupcakes, 12)
+        Pair(R.string.twelve_cupcakes, 12),
+
     )
+
+    val toppings = listOf(
+        Pair("None", 0.00),
+        Pair("Sprinkles", 0.50),
+        Pair("Chocolate Chips", 0.75),
+        Pair("Whipped Cream", 1.00),
+        Pair("Crushed Oreos", 1.25)
+    )
+
 }
