2481506146dcab828be7632dea9d3b3489c640f0
Abiye12 <a.opusunju2269@student.leedsbeckett.ac.uk>
2026-04-29 16:49:37 +0100

    Add Navigation and Lifecycle dependencies

--- StudentApplication/app/build.gradle.kts
+++ StudentApplication/app/build.gradle.kts
@@ -45,6 +45,9 @@ android {
 }
 
 dependencies {
+    implementation(libs.androidx.navigation.compose)
+    implementation(libs.androidx.lifecycle.viewmodel.compose)
+    implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.activity.compose)
     implementation(platform(libs.androidx.compose.bom))
