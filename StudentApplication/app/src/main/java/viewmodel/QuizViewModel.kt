e87472d49566cc2c5d7f28d5762559ae967ae4d1
Abiye12 <a.opusunju2269@student.leedsbeckett.ac.uk>
2026-04-29 17:35:01 +0100

    Add quiz state fields to QuizViewModel

--- StudentApplication/app/src/main/java/viewmodel/QuizViewModel.kt
+++ StudentApplication/app/src/main/java/viewmodel/QuizViewModel.kt
@@ -1,5 +1,9 @@
 package com.dma.studentapplication.viewmodel
 
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
+import com.dma.studentapplication.model.Question
 import android.app.Application
 import androidx.lifecycle.AndroidViewModel
 import com.dma.studentapplication.data.QuizRepository
@@ -7,6 +11,20 @@ import com.dma.studentapplication.data.QuizRepository
 class QuizViewModel(application: Application) : AndroidViewModel(application) {
 
     private val repository = QuizRepository(application.applicationContext)
+    var questions by mutableStateOf<List<Question>>(emptyList())
+        private set
+
+    var currentQuestionIndex by mutableStateOf(0)
+        private set
+
+    var score by mutableStateOf(0)
+        private set
+
+    var selectedAnswerIndex by mutableStateOf(-1)
+        private set
+
+    var isAnswered by mutableStateOf(false)
+        private set
 
     fun getTopics(): List<String> {
         return repository.getTopics()
