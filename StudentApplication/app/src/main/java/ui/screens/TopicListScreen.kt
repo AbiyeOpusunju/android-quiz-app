4d8fcd5971b1ac61ecc54b58f5e74f718001e1fa
Abiye12 <a.opusunju2269@student.leedsbeckett.ac.uk>
2026-04-29 17:46:30 +0100

    Create TopicListScreen composable with parameters

---
+++ StudentApplication/app/src/main/java/ui/screens/TopicListScreen.kt
@@ -0,0 +1,10 @@
+package com.dma.studentapplication.ui.screens
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun TopicListScreen(
+    topics: List<String>,
+    onTopicSelected: (String) -> Unit
+) {
+}
