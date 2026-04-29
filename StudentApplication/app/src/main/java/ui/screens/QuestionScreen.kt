d4e6f82d5ab28893443b114841d244e0962b2e5c
Abiye12 <a.opusunju2269@student.leedsbeckett.ac.uk>
2026-04-29 18:06:19 +0100

    Add imports to QuestionScreen

--- StudentApplication/app/src/main/java/ui/screens/QuestionScreen.kt
+++ StudentApplication/app/src/main/java/ui/screens/QuestionScreen.kt
@@ -2,6 +2,16 @@ package com.dma.studentapplication.ui.screens
 
 import androidx.compose.runtime.Composable
 import com.dma.studentapplication.model.Question
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedButton
+import androidx.compose.material3.Text
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 
 @Composable
 fun QuestionScreen(
