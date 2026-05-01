9677e39c8a143c83d8dc89ffdcf276685c09f244
Abiye12 <a.opusunju2269@student.leedsbeckett.ac.uk>
2026-05-01 18:49:34 +0100

    Create HistoryDetailScreen composable with parameters

---
+++ StudentApplication/app/src/main/java/com/dma/studentapplication/ui/screens/HistoryDetailScreen.kt
@@ -0,0 +1,11 @@
+package com.dma.studentapplication.ui.screens
+
+import androidx.compose.runtime.Composable
+import com.dma.studentapplication.data.QuizResult
+
+@Composable
+fun HistoryDetailScreen(
+    result: QuizResult,
+    onBackClicked: () -> Unit
+) {
+}
