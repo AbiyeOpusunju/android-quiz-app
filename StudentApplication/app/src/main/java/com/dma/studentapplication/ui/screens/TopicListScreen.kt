5c819bb53b16838b651780d665e0bc3a47eb0be9
Abiye12 <a.opusunju2269@student.leedsbeckett.ac.uk>
2026-05-01 19:28:33 +0100

    Wrap title and topic list in inner Column for layout spacing and Fix TopicListScreen layout and add Quiz History button

--- StudentApplication/app/src/main/java/com/dma/studentapplication/ui/screens/TopicListScreen.kt
+++ StudentApplication/app/src/main/java/com/dma/studentapplication/ui/screens/TopicListScreen.kt
@@ -14,6 +14,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.statusBarsPadding
 
 @Composable
 fun TopicListScreen(
@@ -24,28 +25,39 @@ fun TopicListScreen(
     Column(
         modifier = Modifier
             .fillMaxSize()
+            .statusBarsPadding()
             .padding(16.dp),
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.SpaceBetween
-
-
     ) {
-        Text(
-            text = "Android Topics",
-            style = MaterialTheme.typography.headlineLarge,
-            modifier = Modifier.padding(vertical = 24.dp)
-        )
-        LazyColumn {
-            items(topics) { topic ->
-                Button(
-                    onClick = { onTopicSelected(topic) },
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .padding(vertical = 4.dp)
-                ) {
-                    Text(text = topic.replaceFirstChar { it.uppercase() })
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text(
+                text = "Android Topics",
+                style = MaterialTheme.typography.headlineLarge,
+                modifier = Modifier.padding(vertical = 24.dp)
+            )
+            LazyColumn {
+                items(topics) { topic ->
+                    Button(
+                        onClick = { onTopicSelected(topic) },
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(vertical = 4.dp)
+                    ) {
+                        Text(text = topic.replaceFirstChar { it.uppercase() })
+                    }
                 }
             }
+        }
+        Button(
+            onClick = onHistoryClicked,
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(bottom = 16.dp)
+        ) {
+            Text(text = "Quiz History")
         }
     }
 }
